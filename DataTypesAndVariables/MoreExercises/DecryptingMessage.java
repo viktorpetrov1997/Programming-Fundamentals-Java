b4f29bd291ddbae42f6c73e100324c97e65dde7f
Viktor Petrov <viktor.petrov0497@gmail.com>
2023-03-02 22:21:40 +0200

    Add exercises

---
+++ DataTypesAndVariables/MoreExercises/DecryptingMessage.java
@@ -0,0 +1,20 @@
+package data.types.more.exercises;
+
+import java.util.Scanner;
+
+public class DecryptingMessage
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int key = Integer.parseInt(scanner.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for(int i=0;i<n;i++)
+        {
+            char character = (char) (scanner.next().charAt(0) + key);
+            System.out.print(character);
+        }
+    }
+}
