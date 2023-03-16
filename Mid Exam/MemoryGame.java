406c5a46f0e95756f7b454eef2f77275e99efbb1
Viktor Petrov <126717931+viktorpetrov1997@users.noreply.github.com>
2023-03-16 16:01:08 +0100

    Add exercises

---
+++ Mid Exam/MemoryGame.java
@@ -0,0 +1,65 @@
+package mid.exam;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class MemoryGame
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> numbers = Arrays.stream(scanner.nextLine().split(" ")).collect(Collectors.toList());
+
+        int numberOfMoves = 0;
+        String input = scanner.nextLine();
+        while(!input.equals("end"))
+        {
+            ++numberOfMoves;
+            String[] commandArray = input.split(" ");
+            int firstIndex = Integer.parseInt(commandArray[0]);
+            int secondIndex = Integer.parseInt(commandArray[1]);
+            int biggerIndex = Math.max(firstIndex,secondIndex);
+            int smallerIndex = Math.min(firstIndex,secondIndex);
+
+            if(firstIndex == secondIndex || (firstIndex < 0 || firstIndex > numbers.size() - 1)
+            || (secondIndex < 0 || secondIndex > numbers.size() - 1))
+            {
+                System.out.println("Invalid input! Adding additional elements to the board");
+                int middleOfList = numbers.size() / 2;
+                numbers.add(middleOfList,"-" + numberOfMoves + "a");
+                numbers.add(middleOfList + 1,"-" + numberOfMoves + "a");
+            }
+            else
+            {
+                if(numbers.get(firstIndex).equals(numbers.get(secondIndex)))
+                {
+                    System.out.printf("Congrats! You have found matching elements - %s!%n",numbers.get(firstIndex));
+                    numbers.remove(biggerIndex);
+                    numbers.remove(smallerIndex);
+                    if(numbers.isEmpty())
+                    {
+                        System.out.printf("You have won in %d turns!%n",numberOfMoves);
+                        break;
+                    }
+                }
+                else
+                {
+                    System.out.println("Try again!");
+                }
+            }
+
+            input = scanner.nextLine();
+        }
+        if(!numbers.isEmpty())
+        {
+            System.out.println("Sorry you lose :(");
+            for(String element: numbers)
+            {
+                System.out.print(element + " ");
+            }
+        }
+    }
+}
---
+++ Mid Exam/TheLift.java
@@ -0,0 +1,55 @@
+package mid.exam;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class TheLift
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int people = Integer.parseInt(scanner.nextLine());
+        List<Integer> wagons = Arrays.stream(scanner.nextLine().split(" "))
+                        .map(Integer::parseInt).collect(Collectors.toList());
+
+        int currentCapacity = 0;
+        for (int i = 0; i < wagons.size(); i++)
+        {
+            currentCapacity += wagons.get(i);
+        }
+
+        int maxCapacity = wagons.size() * 4;
+
+        for (int i = 0; i < wagons.size(); i++)
+        {
+            if(wagons.get(i) < 4 && people > 0)
+            {
+                while(wagons.get(i) < 4 && people > 0)
+                {
+                    wagons.set(i,wagons.get(i) + 1);
+                    ++currentCapacity;
+                    --people;
+                }
+            }
+            if(currentCapacity >= maxCapacity || people <= 0)
+            {
+                break;
+            }
+        }
+        if(people <= 0 && currentCapacity < maxCapacity)
+        {
+            System.out.println("The lift has empty spots!");
+        }
+        if(people > 0 && currentCapacity >= maxCapacity)
+        {
+            System.out.printf("There isn't enough space! %d people in a queue!%n",people);
+        }
+        for(int wagon : wagons)
+        {
+            System.out.print(wagon + " ");
+        }
+    }
+}
