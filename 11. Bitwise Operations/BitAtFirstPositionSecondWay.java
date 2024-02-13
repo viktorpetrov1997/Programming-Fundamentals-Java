d89cf17b7e3e17bd720ebb96aa33765e09458c46
Viktor Petrov <126717931+viktorpetrov1997@users.noreply.github.com>
2024-02-13 20:49:30 +0200

    Add exercises

---
+++ 11. Bitwise Operations/BitAtFirstPositionSecondWay.java
@@ -0,0 +1,19 @@
+package BitwiseOperations;
+
+import java.util.Scanner;
+
+public class BitAtFirstPositionSecondWay
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int n, bitAtPosition1;
+
+        n = scanner.nextInt();
+
+        bitAtPosition1 = (n >> 1) & 1;
+
+        System.out.println(bitAtPosition1);
+    }
+}
