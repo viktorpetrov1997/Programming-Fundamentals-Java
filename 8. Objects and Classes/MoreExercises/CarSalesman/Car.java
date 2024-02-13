dc9baa9736f914c7626b1df8e56d70e2c712cc93
Viktor Petrov <126717931+viktorpetrov1997@users.noreply.github.com>
2024-02-13 20:48:36 +0200

    Add exercises

---
+++ 8. Objects and Classes/MoreExercises/CarSalesman/Car.java
@@ -0,0 +1,40 @@
+package ObjectsAndClasses.MoreExercises.CarSalesman;
+
+public class Car
+{
+    String model;
+    Engine engine;
+    String weight;
+    String color;
+
+    public Car(String model, Engine engine)
+    {
+        this.model = model;
+        this.engine = engine;
+        this.weight = "n/a";
+        this.color = "n/a";
+    }
+
+    public void setWeight(String weight)
+    {
+        this.weight = weight;
+    }
+
+    public void setColor(String color)
+    {
+        this.color = color;
+    }
+
+    public Engine getEngine()
+    {
+        return engine;
+    }
+
+    @Override
+    public String toString()
+    {
+        Engine eng = this.getEngine();
+        return String.format("%s:%n  %s:%n    Power: %s%n    Displacement: %s%n    Efficiency: %s%n  Weight: %s%n  Color: %s",
+                this.model,eng.getModel(),eng.getPower(),eng.getDisplacement(),eng.getEfficiency(),this.weight,this.color);
+    }
+}
---
+++ 8. Objects and Classes/Songs.java
@@ -0,0 +1,48 @@
+package ObjectsAndClasses;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Songs
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfSongs = Integer.parseInt(scanner.nextLine());
+
+        List<String> typeList = new ArrayList<>();
+        List<String> name = new ArrayList<>();
+        List<String> time = new ArrayList<>();
+        for (int i = 0; i < numberOfSongs; i++)
+        {
+            String song = scanner.nextLine();
+            String[] songArray = song.split("_");
+
+            typeList.add(songArray[0]);
+            name.add(songArray[1]);
+            time.add(songArray[2]);
+        }
+        String type = scanner.nextLine();
+        if(type.equals("all"))
+        {
+            for (int i = 0; i < name.size(); i++)
+            {
+                System.out.println(name.get(i));
+            }
+        }
+        else
+        {
+            for (int i = 0; i < name.size(); i++)
+            {
+                if(typeList.get(i).equals(type))
+                {
+                    System.out.println(name.get(i));
+                }
+            }
+        }
+    }
+}
