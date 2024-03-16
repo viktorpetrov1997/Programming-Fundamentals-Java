5a16b4fd0d7d18877c29dd7a0cfb49ba920f4cec
Viktor Petrov <126717931+viktorpetrov1997@users.noreply.github.com>
2024-03-16 02:31:57 +0200

    Add exercises

---
+++ 9. Maps, Lambda and Stream API/Exercises/CompanyUsers.java
@@ -0,0 +1,48 @@
+package MapsLambdaAndStreamAPI.Exercises;
+
+import java.util.*;
+
+public class CompanyUsers
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        LinkedHashMap<String, List<String>> companyEmployees = new LinkedHashMap<String, List<String>>();
+
+        String input = scanner.nextLine();
+        while(!input.equals("End"))
+        {
+            String[] companyInfo = input.split(" -> ");
+            String company = companyInfo[0];
+            String employeeId = companyInfo[1];
+            companyEmployees.putIfAbsent(company,new ArrayList<>());
+            if(!companyEmployees.get(company).contains(employeeId))
+            {
+                companyEmployees.get(company).add(employeeId);
+            }
+            input = scanner.nextLine();
+        }
+
+        /*for (Map.Entry<String, List<String>> entry : companyEmployees.entrySet())
+        {
+            System.out.println(entry.getKey());
+            for (String employeeId : entry.getValue())
+            {
+                System.out.println("-- " + employeeId);
+            }
+        }*/
+
+        /*companyEmployees.entrySet().stream()
+                .forEach(company -> {
+                    System.out.println(company.getKey());
+                    company.getValue().forEach(employee -> System.out.println("-- " + employee));
+                });*/
+
+        companyEmployees.forEach((key, value) ->
+        {
+            System.out.println(key);
+            value.forEach(employee -> System.out.println("-- " + employee));
+        });
+    }
+}
