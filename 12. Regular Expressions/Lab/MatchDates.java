ba0a37fbfe5e4d509fa586a390b430ce25522fa4
Viktor Petrov <126717931+viktorpetrov1997@users.noreply.github.com>
2024-03-16 02:37:43 +0200

    Add exercises

---
+++ 12. Regular Expressions/Lab/MatchDates.java
@@ -0,0 +1,27 @@
+package RegularExpressions.Lab;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchDates
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        String regex = "(?<day>\\d{2})([.\\/-])(?<month>[A-Z][a-z]{2})\\2(?<year>\\d{4})";
+        String text = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find())
+        {
+            System.out.printf("Day: %s, Month: %s, Year: %s%n",
+                              matcher.group("day"),
+                              matcher.group("month"),
+                              matcher.group("year"));
+        }
+    }
+}
---
+++ 12. Regular Expressions/Lab/MatchFullName.java
@@ -0,0 +1,24 @@
+package RegularExpressions.Lab;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchFullName
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        String regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+\\b";
+        String text = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find())
+        {
+            System.out.print(matcher.group() + " ");
+        }
+    }
+}
